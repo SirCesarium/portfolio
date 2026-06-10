c4a91b8587d4bac1e89d2a0ee27f7fc083fe63f0
SirCesarium <239650612+SirCesarium@users.noreply.github.com>
2026-06-10 12:35:14 -0400

    feat: add smooth scrolling to html

--- app/layout.tsx
+++ app/layout.tsx
@@ -25,7 +25,7 @@ export default function RootLayout({
   return (
     <html
       lang="en"
-      className={`${geistSans.variable} ${jetbrainsMono.variable} h-full antialiased`}
+      className={`${geistSans.variable} ${jetbrainsMono.variable} h-full antialiased scroll-smooth`}
     >
       <head>
         <link
